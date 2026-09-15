efb9cba6da7ef2ed2958866b8c710d240f8b0ba6
Nicolina Wegert <nicolinawegert@gmail.com>
2026-09-15 13:21:31 +0200

    Add taskService with functions to fetch and create tasks

---
+++ src/services/taskService.js
@@ -0,0 +1,27 @@
+const API_URL = 'http://localhost:5035/api/tasks'
+
+export async function getTasks() {
+  const response = await fetch(API_URL)
+
+  if (!response.ok) {
+    throw new Error('Could not fetch tasks')
+  }
+
+  return await response.json()
+}
+
+export async function createTask(task) {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  if (!response.ok) {
+    throw new Error('Could not create task')
+  }
+
+  return await response.json()
+}
